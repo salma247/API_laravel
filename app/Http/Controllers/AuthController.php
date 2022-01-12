f3745edeaa7a16a60a074976b700d6324e9322b6
Salma Ahmed <55872989+salma247@users.noreply.github.com>
2022-01-12 20:50:00 +0200

    Add files via upload

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            "name"=>"required|string",
+            "email"=>"requierd|string|unique:users,email",
+            "password"=>"requierd|string|confirmed"
+        ]);
+
+        $user = User::create([
+            'name'=>$fields['name'],
+            'email'=>$fields['email'],
+            'password'=>bcrypt($fields['password'])
+        ]);
+        $token = $user->createToken("myAppToken")->plainTextToken;
+        $response = [
+            "user"=>$user,
+            "token"=>$token
+        ];
+
+        return response($response,201);
+
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            "email"=>"requierd|string|unique:users,email",
+            "password"=>"requierd|string|confirmed"
+        ]);
+        $user = User::where("email",$fields['email'])->first();
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response(["message" => "bad login"],401);
+        }
+        $token = $user->createToken("myAppToken")->plainTextToken;
+        $response = [
+            "user"=>$user,
+            "token"=>$token
+        ];
+
+        return response($response,201);
+    }
+
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+        return;
+    }
+
+
+}
